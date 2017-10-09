8fe70fb91d37325a2f1da20d7e5b2fd6f42a9b63
Med <Med@MED-PC>
2017-10-09 18:19:32 +0100

    Checkin exercises

---
+++ 4-microsoft-web-technology-comparison-m4-exercise-files/DemoMVC/DemoMVC/Models/TechItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMVC.Models
+{
+    public class TechItem
+    {
+        public string Name { get; set; }
+
+        public static IEnumerable<TechItem> GetItems()
+        {
+            return new List<TechItem>
+            {
+                new TechItem {Name="ASP" },
+                new TechItem {Name="ASP.NET" },
+                new TechItem {Name="ASP.NET Core" }
+
+            };
+        }
+    }
+}
